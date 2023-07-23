632734c533e8990fb0ab6508170e16c622319171
Temberveni Harshitha <harshithat2304@gmail.com>
2023-07-23 17:27:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/Navbar'
+import {Routes,Route} from "react-router-dom";
+import Home from './routes/Home';
+import Contact from './routes/Contact';
+import About from './routes/About';
+import Hair from './routes/Hair';
+import Dental from './routes/Dental';
+import Laser from './routes/Laser';
+import Skin from './routes/Skin';
+import PermanentMakeup from './routes/PermanentMakeup';
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/Skin" element={<Skin/>}/>
+        <Route path="/Laser" element={<Laser/>}/>
+        <Route path="/Dental" element={<Dental/>}/>
+        <Route path="/Hair" element={<Hair/>}/>
+        <Route path="/SemipermanentMakeup" element={<PermanentMakeup/>}/>
+        <Route path="/Contact" element={<Contact/>}/>
+      </Routes>
+      
+      
+      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ChakraProvider } from '@chakra-ui/react'
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
-
+import {ThemeProvider} from "@material-tailwind/react";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
+  <ChakraProvider>
+    <ThemeProvider>   
     <App />
-  </React.StrictMode>
+  </ThemeProvider> 
+  </ChakraProvider>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
